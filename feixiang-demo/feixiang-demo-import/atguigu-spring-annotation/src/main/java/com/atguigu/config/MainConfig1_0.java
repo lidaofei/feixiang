0fde989ed153a24f51f203343db10bbbc66f2cb5
lidaofei <392586954@qq.com>
2019-12-20 12:55:38 +0800

    添加spring annotation demo

---
+++ feixiang-demo/feixiang-demo-import/atguigu-spring-annotation/src/main/java/com/atguigu/config/MainConfig1_0.java
@@ -0,0 +1,15 @@
+package com.atguigu.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/*
+ * 基础配置
+ * @author lidaofei
+ * @date 2019/12/20 11:21
+ */
+@Configuration
+@ComponentScan(basePackages = "com.atguigu")
+public class MainConfig1_0 {
+
+}
